77a82cd739ee6df90550c6b24c441f46d0fc7831
fumokmm <fumo.kmm@gmail.com>
2020-03-18 20:31:14 +0900

    PowerShellのバージョン情報を追加

---
+++ PowerShell/0001_version.ps1
@@ -0,0 +1,59 @@
+# PowerShellのバージョン情報
+#
+# ref: https://ja.wikipedia.org/wiki/PowerShell#Windows_PowerShell
+#
+# Windows PowerShell
+#
+# Windows PowerShell 1.0
+#   - 2003年9月、コードネーム Monad公開
+#   - 2006年4月、正式名称がWindows PowerShellとなる、リリース候補 (RC) 1がリリース
+#   - 2006年9月、RC2リリース
+#   - 2006年11月、Windows PowerShell 1.0がウェブ上でリリース (RTW)
+#   - 動作には .NET Framework 2.0が必要
+#   - 2007年1月、PowerShell 1.0 for Vistaがリリース
+#
+#   マイクロソフトはこれからの主なGUIツールはPowerShell上に構築されると表明
+#   主な管理機能がスクリプト可能になるとした。
+#   例えば、Exchange Server 2007の管理ツールはPowerShellの上に構築されている。
+#   多くの日常的な場面でPowerShellはcmd.exeやWSHを置き換えるものとして利用できる。
+#
+# Windows PowerShell 2.0
+#   - 2009年10月リリース
+#
+# Windows PowerShell 3.0
+#   - 2012年9月リリース
+#   - 動作には .NET Framework 4が必要
+#
+# Windows PowerShell 4.0
+#   - 2013年10月リリース
+#   - 動作には.NET Framework 4.5が必要
+#
+# Windows PowerShell 5.0
+#   - 2015年12月リリース
+#   - 動作には.NET Framework 4.5が必要
+#
+# Windows PowerShell 5.1
+#   - 2016年8月リリース
+#   - Windows 10 Anniversary Update及びWindows Server 2016の標準バージョン
+#   - Windows PowerShell 5.1 は DesktopとCoreの2つのエディションを提供する
+#   - Desktop Editionは従来どおりのWindows PowerShell
+#   - Core EditionはNano Serverで実行するために、.NET Core上に構築され、いくつかの機能が削減されている
+#
+# PowerShell Core
+# PowerShell Core 6.0
+#   - 2016年8月、オープンソース化、Linux/OS Xへの移植を発表
+#   - 2018年1月、PowerShell Core 6.0リリース
+#   - ランタイムとして.NET Frameworkに代わり.NET Core 2.0を使用
+#   - これによりPowerShell Core6はWindows、macOS、Linuxで動作が可能になった
+#
+# PowerShell Core 6.1
+#   - 2018年9月、PowerShell Core 6.1リリース
+#   - Windows 10およびWindows Server 2019との互換性強化
+#   - サポートプラットフォームの強化[16]が図られている。.NET Core 2.1を使用
+#
+# PowerShell Core 6.2
+#   - 2019年3月、PowerShell Core 6.2リリース
+#
+# PowerShell 7.0
+#   - 2020年3月、PowerShell 7.0リリース
+#   - .NET Core 3.1を使用
