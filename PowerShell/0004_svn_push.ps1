31b589604ec610b5c03353a99b2cea034ddc37af
fumokmm <fumo.kmm@gmail.com>
2021-06-17 01:16:40 +0900

    ローカルのSVNリポジトリをリモートリポジトリにpushするスクリプトのサンプルを追加。

---
+++ PowerShell/0004_svn_push.ps1
@@ -0,0 +1,111 @@
+$reposName = 'testrepos'
+$remoteBaseDir = 'N:\repos\svn\'
+
+# プッシュを実装
+function doPush {
+    param(
+        $reposName,
+        $remoteBaseDir,
+        $localReposDir,
+        $remoteReposDir
+    )
+    # リポジトリフォルダのコピーを実行 (念のため)
+    robocopy $localReposDir $remoteReposDir /MIR /E /R:1
+    
+    if ($remoteBaseDir.Substring(1, 1) -eq ':') {
+        $remoteReposSvnPath = 'file:///' + ($remoteReposDir -replace '\\', '/') + '/trunk'
+    } else {
+        $remoteReposSvnPath = 'file:' + ($remoteReposDir -replace '\\', '/') + '/trunk'
+    }
+
+    # TortoiseSvnのRepository Browserのショートカットを作成
+    $wshShell = New-Object -ComObject WScript.Shell
+    $linkPath = Join-Path (Join-Path $remoteBaseDir $reposName) ($reposName + '(SVN).lnk')
+    $shortcut = $wshShell.CreateShortcut($linkPath)
+    $shortcut.TargetPath = "`"C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe`""
+    $shortcut.Arguments = "/command:repobrowser /path:`"$($remoteReposSvnPath)`""
+    $shortcut.IconLocation = "C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe"
+    $shortcut.WorkingDirectory = "C:\Program Files\TortoiseSVN\bin"
+    $shortcut.Save()
+}
+
+# リポジトリのリビジョン番号を調べる
+# リポジトリがない場合、リビジョン0を返却
+function getReposRevision {
+    param(
+        $reposDir # リポジトリのディレクトリ
+    )
+    if (($reposDir -ne $null) -and (Test-Path $reposDir) -eq $True) {
+        return [int](Get-Content (Join-Path (Join-Path $reposDir 'db') 'current'))
+    } else {
+        return [int]0
+    }
+}
+
+
+# 同期化したリビジョンを更新
+function updateSyncRevision{
+    param(
+        $newRevision
+    )
+    Set-Content -Path (Join-Path $PSScriptRoot \..\.syncrevision) -Value $newRevision
+}
+
+######################################
+# ここからメイン処理
+######################################
+
+$localReposDir = Resolve-Path (Join-Path $PSScriptRoot \..\repos)
+$remoteReposDir = Join-Path (Join-Path $remoteBaseDir $reposName) 'repos'
+
+# リポジトリのリビジョンを調べる
+$localRevision = getReposRevision -reposDir $localReposDir
+$remoteRevision = getReposRevision -reposDir $remoteReposDir
+if ((Test-Path (Join-Path $PSScriptRoot \..\.syncrevision))) {
+    $syncRevision = [int](Get-Content (Join-Path $PSScriptROot \..\.syncrevision))
+} else {
+    $syncRevision = [int]0
+}
+Write-Host "local  : $localRevision"
+Write-Host "remote : $remoteRevision"
+Write-Host "sync   : $syncRevision"
+
+# 前回同期時からローカルリビジョンが同じ場合、push不要
+if ($localRevision -eq $syncRevision) {
+    Write-Host '前回同期化時からローカルリポジトリに更新がないため、Pushの必要がありません。'
+    exit 0
+
+# 前回同期化時よりもローカルリビジョンが下がった場合、状態が変
+} elseif ($localRevision -lt $syncRevision) {
+    Write-Host "状態が変です。前回同期化時よりもローカルリポジトリが古くなっています。前回同期化時のリビジョン:$syncRevision、ローカルのリビジョン:$localRevision"
+    exit 1
+
+# 前回同期化後にローカルリビジョンが上がった場合、push可能
+} else {
+    # Write-Host 'Push可能です。'
+    
+    # 前回同期化時よりもリモートリビジョンが上がっている場合
+    if ($syncRevision -lt $remoteRevision) {
+        Write-Host 'リモートが最新です。先にPullして下さい。'
+        Write-Host '※Pullするとローカルにコミットした内容は失われますので、データを退避するなりして備えて下さい。'
+        exit 0
+
+    # 前回同期化時よりもリモートリビジョンが下がっている場合
+    } elseif ($syncRevision -gt $remoteRevision) {
+        Write-Host "状態が変です。前回同期時よりもリモートリポジトリが古くなっています。前回同期化時のリビジョン:$syncRevision、リモートのリビジョン:$remoteRevision"
+        exit 1
+
+    # 前回同期化時とリモートリビジョンが同じ場合
+    } else {
+        # Write-Host 'Push可能です。'
+        
+        # プッシュを実行
+        doPush -reposName $reposName `
+               -remoteBaseDir $remoteBaseDir `
+               -localReposDir $localReposDir `
+               -remoteReposDir $remoteReposDir
+
+        # 同期化したリビジョンを更新
+        updateSyncRevision -newRevision $localRevision
+    }
+}
