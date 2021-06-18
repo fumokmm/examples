d8dc4b64f679366edbf804dbb9a39345053933d8
fumokmm <fumo.kmm@gmail.com>
2021-06-18 20:38:50 +0900

    ローカルのSVNリポジトリをローカルにpullするスクリプトのサンプルを追加。

---
+++ PowerShell/0007_svn_pull.ps1
@@ -0,0 +1,103 @@
+$reposName = 'testrepos'
+$remoteBaseDir = 'N:\repos\svn\'
+
+# プルを実装
+function doPull {
+    param(
+        $localReposDir,
+        $remoteReposDir
+    )
+
+    # ローカルリポジトリを退避
+    robocopy $localReposDir (Join-Path $PSScriptRoot "\..\repos_bk\repos_$((Get-Date).ToString('yyyyMMdd_HHmmss'))") /MIR /E /R:1
+    # リポジトリフォルダのコピーを実行
+    robocopy $remoteReposDir $localReposDir /MIR /E /R:1
+}
+
+# リポジトリのリビジョン番号を調べる
+# リポジトリがない場合、リビジョン0を返却
+function getReposRevision {
+    param(
+        $reposDir # リポジトリのディレクトリ
+    )
+    if (($reposDir -ne $null) -and (Test-Path $reposDir) -eq $True) {
+        return [int](Get-Content (Join-Path (Join-Path $reposDir 'db') 'current'))
+    } else {
+        return [int]0
+    }
+}
+
+# 同期化したリビジョンを更新
+function updateSyncRevision{
+    param(
+        $newRevision
+    )
+    Set-Content -Path (Join-Path $PSScriptRoot \..\.syncrevision) -Value $newRevision
+}
+
+######################################
+# ここからメイン処理
+######################################
+
+$localReposDir = Resolve-Path (Join-Path $PSScriptRoot \..\repos)
+$remoteReposDir = Join-Path (Join-Path $remoteBaseDir $reposName) 'repos'
+
+# リポジトリのリビジョンを調べる
+$localRevision = getReposRevision -reposDir $localReposDir
+$remoteRevision = getReposRevision -reposDir $remoteReposDir
+if ((Test-Path (Join-Path $PSScriptRoot \..\.syncrevision))) {
+    $syncRevision = [int](Get-Content (Join-Path $PSScriptROot \..\.syncrevision))
+} else {
+    $syncRevision = [int]0
+}
+Write-Host "local  : $localRevision"
+Write-Host "remote : $remoteRevision"
+Write-Host "sync   : $syncRevision"
+
+# 前回同期時とリモートリビジョンが同じ場合、pull不要
+if ($remoteRevision -eq $syncRevision) {
+    Write-Host '前回同期化時からリモートリポジトリに更新がないため、Pullの必要がありません。'
+    exit 0
+
+# 前回同期化時よりもリモートリビジョンが下がった場合、状態が変
+} elseif ($remoteRevision -lt $syncRevision) {
+    Write-Host "状態が変です。前回同期化時よりもリモートリポジトリが古くなっています。前回同期化時のリビジョン:$syncRevision、リモートのリビジョン:$remoteRevision"
+    exit 1
+
+# 前回同期化後にリモートリビジョンが上がった場合、pull可能
+} else {
+    # Write-Host 'Pull可能です。'
+    
+    # 前回同期化時よりもローカルリビジョンが上がっている場合、確認メッセージを入れてからPullする
+    if ($syncRevision -lt $localRevision) {
+        $message = @"
+リモートが最新ですが、ローカルにもコミットしています。
+ローカル: $($syncRevision) -> $($localRevision)
+リモート: $($syncRevision) -> $($remoteRevision)
+※Pullするとローカルにコミットした内容は失われますので、データを退避するなりして備えて下さい。
+"@
+        Write-Host $message
+        $objYes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "実行する"
+        $objNo = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "やめておく"
+        $objOptions = [System.Management.Automation.Host.ChoiceDescription[]]($objYes, $objNo)
+        $objMessage = @"
+$($message)
+
+実行しますか？
+"@
+        $resultVal = $host.ui.PromptForChoice('実行しますか？', $objMessage, $objOptions, 1)
+        if ($resultVal -ne 0) {
+            Write-Host "処理が中断されました。"
+            exit 0
+        }
+    }
+    
+    # プルを実行
+    doPull -localReposDir $localReposDir `
+           -remoteReposDir $remoteReposDir
+
+    # 同期化したリビジョンを更新
+    updateSyncRevision -newRevision $remoteRevision
+
+    exit 0
+}
