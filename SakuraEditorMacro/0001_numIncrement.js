86838495e517104bb5012fe62ec18d6493d840a0
fumokmm <fumo.kmm@gmail.com>
2021-07-16 09:08:03 +0900

    numIncrement と numDecrementのサンプルを追加

---
+++ SakuraEditorMacro/0001_numIncrement.js
@@ -0,0 +1,12 @@
+var e = Editor;
+var selectedString = e.GetSelectedString(0);
+// ‘I‘ğó‘Ô‚È‚ç
+if (e.IsTextSelected() == 1) {
+  var num = Number(selectedString);
+  num++;
+  e.Delete();
+  e.InsText(num);
+  // ‘I‘ğ”ÍˆÍ‚Ì•œŒ³
+  e.Left();
+  e.SelectWord();
+}
