8d522408e109d2b06046fd1715519a31e3aa3baf
fumokmm <fumo.kmm@gmail.com>
2021-07-20 21:46:38 +0900

    デフォルト値のサンプルを追加

---
+++ PowerShell/0011_function_args_default_value.ps1
@@ -0,0 +1,13 @@
+﻿# デフォルト値
+function foo($arg = 10) {
+    Write-Host "値は$($arg)です。"
+}
+function bar($arg1, $arg2 = 10) {
+    Write-Host "値は$($arg1)と$($arg2)です。"
+}
+
+foo
+foo 20
+
+bar 15
+bar 15 25
