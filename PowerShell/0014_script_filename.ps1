68c8a6ec04196dfd90f15cd2582b2c8af0cd1e16
fumokmm <fumo.kmm@gmail.com>
2021-07-25 01:20:16 +0900

    サンプルを追加

--- PowerShell/0014_script_filename.ps1
+++ PowerShell/0014_script_filename.ps1
@@ -1,7 +1,11 @@
 ﻿# スクリプトファイル(.ps1)のパス
-$scriptName = Split-Path -Leaf $PSCommandPath
-Write-Host $scriptName
+$scriptName1 = Split-Path -Leaf $PSCommandPath
+Write-Host $scriptName1
+
+# スクリプトファイル(.ps1)のパス
+$scriptName2 = Split-Path -Leaf $Script:MyInvocation.MyCommand.Path
+Write-Host $scriptName2
 
 # スクリプトファイル名
-$scriptName = $Script:MyInvocation.MyCommand.Name
-Write-Host $scriptName
+$scriptName3 = $Script:MyInvocation.MyCommand.Name
+Write-Host $scriptName3
