e2fc310034a8dc6c6593101305a2e4ea7de35695
fumokmm <fumo.kmm@gmail.com>
2021-07-25 01:12:21 +0900

    スクリプトファイル名取得のサンプルを追加

---
+++ PowerShell/0014_script_filename.ps1
@@ -0,0 +1,7 @@
+﻿# スクリプトファイル(.ps1)のパス
+$scriptName = Split-Path -Leaf $PSCommandPath
+Write-Host $scriptName
+
+# スクリプトファイル名
+$scriptName = $Script:MyInvocation.MyCommand.Name
+Write-Host $scriptName
