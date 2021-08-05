cb20543ef4f2b3f7308340eae8e4e36ac65f4888
fumokmm <fumo.kmm@gmail.com>
2021-08-05 20:25:06 +0900

    追加

---
+++ PowerShell/0018_strict_mode.ps1
@@ -0,0 +1,12 @@
+﻿# PS C:\Users\fumo> Set-PSDebug -strict
+# PS C:\Users\fumo> $newValue
+# 変数 '$newValue' は、設定されていないために取得できません。
+# 発生場所 行:1 文字:1
+# + $newValue
+# + ~~~~~~~~~
+#     + CategoryInfo          : InvalidOperation: (newValue:String) []、RuntimeException
+#     + FullyQualifiedErrorId : VariableIsUndefined
+# 
+# PS C:\Users\fumo> Set-PSDebug -Off
+# PS C:\Users\fumo> $newValue
+# PS C:\Users\fumo>
