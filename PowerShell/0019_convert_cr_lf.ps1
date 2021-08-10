2e899ba924729e1c04c9b92a79efa9894cbb085d
fumokmm <fumo.kmm@gmail.com>
2021-08-10 23:12:37 +0900

    PowerShellでエスケープシーケンスと改行コードを変換する のサンプルコード

---
+++ PowerShell/0019_convert_cr_lf.ps1
@@ -0,0 +1,17 @@
+﻿Set-PSDebug -strict
+
+# エスケープシーケンス \n を改行コードへ変換
+$esc = "aaa\nbbb\nccc"
+Write-Host "1: $($esc)"
+$esc2 = $esc -replace '\\n', "`r`n"
+Write-Host "2: $($esc2)"
+
+# 改行コード \n をエスケープシーケンスへ変換
+$crlf = @"
+aaa
+bbb
+ccc
+"@
+Write-Host "3: $($crlf)"
+$crlf2 = $crlf -replace "`r`n", "\n"
+Write-Host "4: $($crlf2)"
