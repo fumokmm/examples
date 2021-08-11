04588c96352d103b40c781d7283f9cd316f5df2b
fumokmm <fumo.kmm@gmail.com>
2021-08-12 00:36:13 +0900

    サイズ単位の定数のサンプルを追加

---
+++ PowerShell/0020_size_units_constants.ps1
@@ -0,0 +1,8 @@
+﻿Set-PSDebug -strict
+
+# cf) https://powershellmagazine.com/2013/05/20/converting-to-size-units-kb-mbgbtb-and-pb-without-using-powershell-multipliers/
+Write-Host "1KB = $(1KB.ToString('#,#'))バイト"
+Write-Host "1MB = $(1MB.ToString('#,#'))バイト"
+Write-Host "1GB = $(1GB.ToString('#,#'))バイト"
+Write-Host "1TB = $(1TB.ToString('#,#'))バイト"
+Write-Host "1PB = $(1PB.ToString('#,#'))バイト"
