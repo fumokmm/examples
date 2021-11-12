12d02792530a0a6d42b5ab9cc0a374abcf6db8e3
fumokmm <fumo.kmm@gmail.com>
2021-11-12 20:41:52 +0900

    if文のサンプルを追加

---
+++ Python/0001_if.py
@@ -0,0 +1,11 @@
+home = "日本"
+if home == "日本":
+    print("Hello, Japan!")
+elif home == "タイ":
+    print("Hello, Thailad!")
+elif home == "インド":
+    print("Hello, India!")
+elif home == "中国":
+    print("Hello, China!")
+else:
+    print("Hello, World!")
