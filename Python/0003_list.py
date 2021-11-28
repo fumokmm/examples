2eb49552d24de18d0860cb599b3528fdb1e32c14
fumokmm <fumo.kmm@gmail.com>
2021-11-28 23:41:08 +0900

    長さチェックを追加

---
+++ Python/0003_list.py
@@ -0,0 +1,7 @@
+# 長さチェックには len関数を使う
+list = ["A", "B", "C", "D", "E"]
+assert len(list) == 5
+
+# リストに要素が含まれているかのチェックには in を使う
+list = ["A", "B", "C", "D", "E"]
+assert ("C" in list) == True
--- Python/0003_list_include.py
+++
@@ -1,2 +0,0 @@
-list = ["A", "B", "C", "D", "E"]
-assert ("C" in list) == True
